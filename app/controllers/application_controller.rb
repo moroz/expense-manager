1608e2895a4a3a22105777136371afe1bd018c35
Karol Moroz <dmuhafc@gmail.com>
2016-11-18 16:14:22 +0800

    Remove WelcomeController
    Removes the WelcomeController. All content is now served by the
    ApplicationController and the main layout is angular.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,11 @@ require "application_responder"
 
 class ApplicationController < ActionController::Base
   self.responder = ApplicationResponder
-  respond_to :html
+  respond_to :json
 
   protect_from_forgery with: :exception
+
+  def angular
+    render 'layouts/angular'
+  end
 end
