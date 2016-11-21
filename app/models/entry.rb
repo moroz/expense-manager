091d65d4069fdc176c1ca022118bd2f7b38cf39e
Karol Moroz <dmuhafc@gmail.com>
2016-11-21 14:08:35 +0800

    Add specs for Entry scopes

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,6 +2,11 @@ class Entry < ApplicationRecord
   belongs_to :account
   after_create :update_account_balance
 
+  scope :date, ->(date) { where(created_at: date.beginning_of_day..date.end_of_day) }
+  scope :today, -> { date(Date.today) }
+  scope :month, ->(month) { where(created_at: month.beginning_of_month..month.end_of_month) }
+  scope :this_month, -> { month(Date.today) }
+
   private 
 
   def update_account_balance
--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Entry, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :all
+
+  describe "scopes" do
+    describe "date" do
+      subject { Entry.date(Date.yesterday) }
+      it { is_expected.to include(entries(:yesterday)) }
+      it { is_expected.not_to include(entries(:today)) }
+    end
+
+    describe "today" do
+      subject { Entry.today }
+      it { is_expected.to include(entries(:today)) }
+      it { is_expected.not_to include(entries(:yesterday)) }
+    end
+
+    describe "month" do
+      subject { Entry.month(1.month.ago.to_date) }
+      it { is_expected.to include(entries(:last_month)) }
+      it { is_expected.not_to include(entries(:today)) }
+    end
+
+    describe "this_month" do
+      subject { Entry.this_month }
+      it { is_expected.to include(entries(:today)) }
+      it { is_expected.not_to include(entries(:last_month)) }
+    end
+  end
+
 end
