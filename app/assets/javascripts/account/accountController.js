0a15dbbd750aea55d25a27c18243c5078ee92039
Karol Moroz <dmuhafc@gmail.com>
2016-11-22 15:24:37 +0800

    Refactor app.js

--- app/assets/javascripts/account/accountController.js
+++ app/assets/javascripts/account/accountController.js
@@ -1,11 +1,10 @@
 angular.module('MeiQian')
   .controller('AccountController', ['account', '$state', '$scope', function(account, $state, $scope) {
     $scope.account = account;
-    $scope.income = $state.params.income;
     $scope.submitEntry = function() {
       if (!$scope.entry_amount || !$scope.entry_description || $scope.entry_amount === '' || $scope.entry_description === '') { return; }
       amount = parseFloat($scope.entry_amount);
-      if (!$scope.income) {
+      if (!$state.current.data.income) {
         amount = -amount;
       }
       account.addEntry($scope.entry_description, amount)
--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,4 +1,4 @@
-angular.module('MeiQian', ['ui.router', 'templates'])
+angular.module('MeiQian', ['ui.router', 'templates', 'Devise'])
   .config(['$stateProvider', '$urlRouterProvider',
     function($stateProvider, $urlRouterProvider) {
       $stateProvider.state('home', {
@@ -15,27 +15,20 @@ angular.module('MeiQian', ['ui.router', 'templates'])
               if (!account.loaded) {      // only request account.json from the server if it's not loaded yet
                 return account.getFromServer();
               }
-              else {
-                return true;
-              }
             }]
-          }
-        })
-        .state('newIncome', {
-          url: '/income',
-          templateUrl: 'account/_new_income.html',
-          controller: 'AccountController',
-          params: {
-            income: true
           }
         })
         .state('newExpense', {
           url: '/expense',
-          templateUrl: 'account/_new_expense.html',
           controller: 'AccountController',
-          params: {
-            income: false
-          }
+          templateUrl: 'account/_new_expense.html',
+          data: { income: false }
+        })
+        .state('newIncome', {
+          url: '/income',
+          controller: 'AccountController',
+          templateUrl: 'account/_new_income.html',
+          data: { income: true }
         });
       $urlRouterProvider.otherwise('account');
     }]);
