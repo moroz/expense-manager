4bc6d2587e63ba3f3850f0152a41cd232947b8f1
Karol Moroz <dmuhafc@gmail.com>
2016-11-24 16:08:42 +0800

    Add more specs for EntriesController

--- spec/controllers/entries_controller_spec.rb
+++ spec/controllers/entries_controller_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe EntriesController, type: :controller do
     sign_in @user
   end
 
+  let(:entry) { FactoryGirl.create(:entry, amount: 100.0, description: "Money", account: @user.account) }
+
   describe "POST #create" do
     it "returns http success" do
       post :create, params: { entry: { amount: -100, description: "Alcohol" } }, format: :json
@@ -15,12 +17,25 @@ RSpec.describe EntriesController, type: :controller do
   end
 
   describe "PUT update" do
-    let!(:entry) { FactoryGirl.create(:entry, amount: 100.0, description: "Money",
-                                      account: @user.account) }
-    
     it "returns http success" do
-      put :update, params: { entry: { id: entry.id, description: "No money", amount: -50 } }
+      put :update, params: { id: entry.id, format: :json, entry: { description: "No money", amount: -50 } }
       expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET show" do
+    context "with an existing entry" do
+      it "returns http success" do
+        get :show, params: { id: entry.id, format: :json }
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context "with a non-existent entry id" do
+      it "returns http error" do
+        get :show, params: { id: 108, format: :json }
+        expect(response).to have_http_status(404)
+      end
     end
   end
 
